ef00c8c39ac0333de8f8fb1fc3d664cd535196a2
Magnus <Magnus.harryson@hotmail.com>
2018-04-24 12:26:54 +0200

    added non-functioning dynamic velocity

--- src/Model.java
+++ src/Model.java
@@ -18,17 +18,20 @@ class Model {
 	final static int ELEMENT_WEIGHT = 2;
 	final static double GRAVITY = 9.82; 
 	
+	public enum Direction {
+		NORTH, EAST, SOUTH, WEST, NOCOLLISION
+	}
 	
-	//random values
-
+	
 	Model(double width, double height) {
 		areaWidth = width;
 		areaHeight = height;
 		
 		
 		// Initialize the model with a few balls
-		balls = ballGenerator(4);
+		balls = ballGenerator(2);
 	}
+	//random values
 	Ball[] ballGenerator(int balls){
 		Ball[] ballArray = new Ball[balls];
 		//ballArray[0] = new Ball(areaWidth/ 3, areaHeight * 0.9, 1.2, 1.6, 0.1);
@@ -38,41 +41,79 @@ class Model {
 		}
 		return ballArray;
 	}
+	void kinEX(Ball bi, Ball bj) {
+		double mi = bi.mass;
+		double mj = bj.mass;
+		double I = mi*bi.vx+mj*bj.vx;
+		double R = -(bj.vx-bi.vx);
+		bj.vx = I-(mi*R)/(mi+mj);
+		bi.vx = bj.vx-R;
+		System.out.println(""+bj.vx+""+bi.vx);
+	}
+	Direction collisionDetection(Ball bi, Ball bj) {
+		if(bj.x > bi.x && bj.x < bi.x2 && bj.y > bi.y && bj.y < bi.y2) {
+			return Direction.NORTH;
+		}
+		else if (bj.x2 > bi.x && bj.x2 < bi.x2 && bj.y > bi.y && bj.y < bi.y2) {
+			return Direction.EAST;
+		}
+		else if (bj.x > bi.x && bj.x < bi.x2 && bj.y2 > bi.y && bj.y2 < bi.y2) {
+			return Direction.SOUTH;
+		}
+		else if (bj.x2 > bi.x && bj.x2 < bi.x2 && bj.y2 > bi.y && bj.y2 < bi.y2) {
+			return Direction.WEST;
+		}
+		else {
+			return Direction.NOCOLLISION;
+		}
+	}
+	void changeDirection (double dirX, double dirY, Ball bi, Ball bj) {
+		if (dirX<0){
+			bi.vx*=-1;
+			bj.vx*=-1;
+		}
+		//Otherwise it depends on which has the higher speed
+		else{
+			if(Math.abs(bi.vx)<Math.abs(bj.vx)){
+				bj.vx*=-1;
+			}
+			else {
+				bi.vx*=-1;
+			}
+		}
+		if (dirY<0){
+			bi.vy*=-1;
+			bj.vy*=-1;
+		}
+		else{
+			if(Math.abs(bi.vy)<Math.abs(bj.vy)){
+				bj.vy*=-1;
+			}
+			else {
+				bi.vy*=-1;
+			}
+		}
+		
+	}
 	void collision(Ball bi, Ball bj){
 		//Implementation of polar velocities makes this deprecated (redundant)
 		double dirX = bi.vx*bj.vx; //Do the balls travel in the same direction?
 		double dirY = bi.vy*bj.vy;
-		double posx1 = bi.x;				//Corner of the ball, x
-		double width = bi.x+(bi.radius*2);	//Width of the ball
-		double posy1 = bi.y;				//Corner of the ball, y
-		double height = bi.y-(bi.radius*2);	//Height of the ball
-		if (bj.x>posx1&&bj.x<width&&bj.y>posy1&&bj.y<height){
-			//The balls meet
-			if (dirX<0){
-				bi.vx*=-1;
-				bj.vx*=-1;
-			}
-			//Otherwise it depends on which has the higher speed
-			else{
-				if(Math.abs(bi.vx)<Math.abs(bj.vx)){
-					bj.vx*=-1;
-				}
-				else {
-					bi.vx*=-1;
-				}
-			}
-			if (dirY<0){
-				bi.vy*=-1;
-				bj.vy*=-1;
-			}
-			else{
-				if(Math.abs(bi.vy)<Math.abs(bj.vy)){
-					bj.vy*=-1;
-				}
-				else {
-					bi.vy*=-1;
-				}
-			}
+		switch(collisionDetection(bi,bj)) {
+			case NORTH:
+				
+				break;
+			case SOUTH:
+				
+				break;
+			case WEST:
+				
+				break;
+			case EAST:
+				kinEX(bi,bj);
+				break;
+			case NOCOLLISION:
+				break;
 		}
 	}
 	//Ball's volume
@@ -88,15 +129,19 @@ class Model {
 		// TODO this method implements one step of simulation with a step deltaT
 		for(int i=0;i<balls.length;i++){
 			Ball bi=balls[i];
+			bi.x2 = bi.x+(bi.radius*2);
+			bi.y2 = bi.y+(bi.radius*2);
 			for(int j=i+1;j<balls.length;j++){
 				Ball bj = balls[j];
+				bj.x2 = bj.x+(bi.radius*2);
+				bj.y2 = bj.y+(bi.radius*2);
 				collision(bi,bj);
 			}
 		}
 
 		for (Ball b : balls) {
 			//Gravitation calculated
-			double deltaG = volume(b.radius)*Model.ELEMENT_WEIGHT*Model.GRAVITY;
+			double deltaG = b.mass*Model.GRAVITY;
 			
 			// compute new position according to the speed of the ball
 			b.vy=b.vy-deltaG;
@@ -145,11 +190,16 @@ class Model {
 			this.vx = vx;
 			this.vy = vy;
 			this.radius = r;
+			this.x2 = this.x+(2*this.radius);
+			this.y2 = this.y+(2*this.radius);
+			this.mass = volume(r)*Model.ELEMENT_WEIGHT;
 		}
 
 		/**
 		 * Position, speed, and radius of the ball. You may wish to add other attributes.
 		 */
 		double x, y, vx, vy, radius;
+		double x2,y2;
+		final double mass;
 	}
 }
